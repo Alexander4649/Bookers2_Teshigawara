363223fec4666260eb9615abc6ca2703ed4e4154
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-06-02 12:11:24 +0000

    14まで-header作成から

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'homes#top' #get 'homes/top'
+  get "/homes/about" => "homes#about", as: "about"
+  get "/homes/top" => "homes#top", as: "top"
+  get "/homes/users/show" => "homes#show", as: "show"
   
   resources :books, only: [:new, :index, :show] #get 'books/new' #get 'books/index' #get 'books/show'
   resources :users, only: [:index, :show, :edit]#get 'users/index',get 'users/show',get 'users/edit'
