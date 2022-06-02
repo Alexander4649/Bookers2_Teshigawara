59c06ca4c98724f52cf2c38633ef8dfcbfb6b6bf
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-06-02 08:38:28 +0000

    [Add]11-必要画面とルートの流れを確認、イメージ湧いてきた

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,18 @@ class User < ApplicationRecord
          #:rememberable（ログイン情報を保存）
          #:validatable（email のフォーマットなどのバリデーション）
          has_many :books, dependent: :destroy#アソシエーション=>1:N
+         
+         
+         
+         has_one_attached :profile_image#プロフィール画像を保存できる設定
+         
+        # プロフィール画像の自動サイズ設定-いづれ使うと
+         #def get_profile_image(width, height)
+         #unless profile_image.attached?
+         #file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+         #profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+         #end
+         #  profile_image.variant(resize_to_limit: [width, height]).processed
+         #end
+         
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root to: 'homes#top' #get 'homes/top'
-  get "/homes/about" => "homes#about", as: "about"
   
-  resources :post_images, only: [:new, :index, :show] #get 'books/new' #get 'books/index' #get 'books/show'
+  resources :books, only: [:new, :index, :show] #get 'books/new' #get 'books/index' #get 'books/show'
+  resources :users, only: [:index, :show, :edit]#get 'users/index',get 'users/show',get 'users/edit'
+  
+  
+  
 end
