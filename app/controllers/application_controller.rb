538aa278c96ba52387241046f708ed013d1a6ebd
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-06-03 09:27:07 +0000

    [Add]15-header作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?#devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行されます。
   #もし、deviseの処理を行う場合、configure_permitted_parametersというものを実行してね」という意味
   
-#  def after_sign_in_path_for(resource)#サインイン後にどこに遷移するかを設定しているメソッド
-#  show_path
-#  end
+  def after_sign_in_path_for(resource)#サインイン後にどこに遷移するかを設定しているメソッド
+  show_path#users/showマイページへ
+  end
 
 
   
-#  def after_sign_out_path_for(resource)#サインアウト後にどこに遷移するかを設定するメソッド
-#19番   top_path
-#  end
+  def after_sign_out_path_for(resource)#サインアウト後にどこに遷移するかを設定するメソッド
+  top_path#homes/topトップページへ
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: 'homes#top' #get 'homes/top'
   get "/homes/about" => "homes#about", as: "about"
   get "/homes/top" => "homes#top", as: "top"
-  get "/homes/users/show" => "homes#show", as: "show"
+  get "/users/show" => "users#show", as: "show"
+  get "/users/index" => "users#index", as: "index"
+  get "/books/index" => "books#index", as: "books_index"
   
   resources :books, only: [:new, :index, :show] #get 'books/new' #get 'books/index' #get 'books/show'
   resources :users, only: [:index, :show, :edit]#get 'users/index',get 'users/show',get 'users/edit'
